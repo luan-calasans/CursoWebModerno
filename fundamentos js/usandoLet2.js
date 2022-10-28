0d1efc800e5ac7f069506c2caee809d66e511fb3
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-10-28 18:10:02 -0400

    Add files via upload

---
+++ fundamentos js/usandoLet2.js
@@ -0,0 +1,10 @@
+let numero = 1
+{
+    let numero2 = 2
+    console.log('dentro =', numero)
+    // se não conseguir encontrar
+    // dentro do escopo a 
+    // variável, irá buscar no 
+    // escopo mais abrangente
+}
+console.log('fora =', numero)
